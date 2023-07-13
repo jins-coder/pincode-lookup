2fed8ac2b47a67bc473264c7ca6f3a9ce3315703
jinson.joseph@cmmtechnologies.in <jinson.joseph@cmmtechnologies.in>
2023-07-13 17:54:56 +0530

    code changed to one-liner

--- js/main.js
+++ js/main.js
@@ -1,91 +1,12 @@
-const getLocationInfo = (e) => {
-	e.preventDefault();
+const getLocationInfo = (e) => { e.preventDefault(); if (pincode.value.length === 0) { alert('Field cannot be empty'); return; } document.querySelector('.locationInfo').innerHTML = ''; (async () => { const response = await fetch(`https://api.postalpincode.in/pincode/${pincode.value}`); const data = await response.json(); data.forEach(dataItems => { if (dataItems.Status !== 'Success' || dataItems.Status === 404) { showIcon('remove'); document.querySelector('.locationInfo').innerHTML = `<article class="message is-danger"><div class="message-header"><p>Location</p><button class="delete" aria-label="delete"></button></div><div class="message-body">${dataItems.Message}</div></article>`; } else { showIcon('check'); const postoffice = dataItems.PostOffice[0]; const pos = Object.keys(postoffice).map(key => postoffice[key] && (`<div class="message-header mb-3"><p>${key}</p></div><div class="message-body"><strong>${postoffice[key]}</strong></div>`)).join(''); document.querySelector('.locationInfo').innerHTML = "<article class='message is-primary'>" + pos + "</article>"; } }); })().catch(error => console.log('Error:', error)); }
 
-	// Fetch the location
-	if (pincode.value.length === 0) {
-		alert('Field cannot be empty');
-		return;
-	}
 
-	document.querySelector('.locationInfo').innerHTML = '';
-	processData().then(r => console.log(r));
 
-}
-async function fetchData() {
-	try {
-		const response = await fetch(`https://api.postalpincode.in/pincode/${pincode.value}`);
-		if (!response.ok) {
-			throw new Error('Network response was not ok');
-		}
-		return await response.json();
-	} catch (error) {
-		console.log('Error:', error.message);
-		// Handle the error appropriately
-	}
-}
-async function processData() {
-	try {
-		const data = await fetchData();
+const showIcon = (status) => { const removeIcon = document.querySelector(".icon-remove"); const checkIcon = document.querySelector(".icon-check"); removeIcon.style.display = "none"; checkIcon.style.display = "none"; const statusIcon = document.querySelector(`.icon-${status}`); statusIcon.style.display = "inline-flex"; }
 
-		data.forEach(dataItems => {
-			if (dataItems.Status !== 'Success' || dataItems.Status === 404) {
-				showIcon('remove');
-				document.querySelector('.locationInfo').innerHTML = `
-          <article class="message is-danger">
-            <div class="message-header">
-              <p>Location</p>
-              <button class="delete" aria-label="delete"></button>
-            </div>
-            <div class="message-body">
-              ${dataItems.Message}
-            </div>
-          </article>
-        `;
-			} else {
-				showIcon('check');
-				const [post_office] = dataItems.PostOffice;
-				const pos = Object.keys(post_office).map(key => {
-					return post_office[key] && (`
-            <div class="message-header mb-3">
-              <p>${key}</p>
-            </div>
-            <div class="message-body">
-              <strong>${post_office[key]}</strong>
-            </div>
-          `);
-				}).join('');
 
-				document.querySelector('.locationInfo').innerHTML = "<article class='message is-primary'>" + pos +
-					"</article>";
-			}
-		});
-	} catch (error) {
-		console.log('Error:', error);
-		// Handle the error appropriately
-	}
-}
+const deleteText = (e) => { if (e.target.className === 'delete') { document.querySelector('.locationInfo').remove(); document.querySelector('.pincode').value = ''; const checkIcon = document.querySelector(".icon-check"); if (checkIcon) { checkIcon.remove(); } } }
 
-const showIcon = (status) => {
-	const removeIcon = document.querySelector(".icon-remove");
-	const checkIcon = document.querySelector(".icon-check");
-
-	removeIcon.style.display = "none";
-	checkIcon.style.display = "none";
-
-	const statusIcon = document.querySelector(`.icon-${status}`);
-	statusIcon.style.display = "inline-flex";
-}
-
-const deleteText = (e) => {
-	if (e.target.className === 'delete') {
-		document.querySelector('.locationInfo').remove();
-		document.querySelector('.pincode').value = '';
-		const checkIcon = document.querySelector(".icon-check");
-		if (checkIcon) {
-			checkIcon.remove();
-		}
-	}
-}
 
 zipform.addEventListener('submit', getLocationInfo);
 document.querySelector('body').addEventListener('click', deleteText);
