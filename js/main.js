dab4c038596905054c87765ed017992d2a3cdbf6
jinson.joseph@cmmtechnologies.in <jinson.joseph@cmmtechnologies.in>
2023-07-13 17:49:59 +0530

    change fetch-then to async await

--- js/main.js
+++ js/main.js
@@ -8,45 +8,61 @@ const getLocationInfo = (e) => {
 	}
 
 	document.querySelector('.locationInfo').innerHTML = '';
+	processData().then(r => console.log(r));
 
-	fetch(`https://api.postalpincode.in/pincode/${pincode.value}`)
-		.then(response => response.json())
-		.then(data => {
-			data.forEach(dataItems => {
-				if (dataItems.Status !== 'Success' || dataItems.Status === 404) {
-					showIcon('remove');
-					const errorMessage = `
-			  <article class="message is-danger">
-				<div class="message-header">
-				  <p>Location</p>
-				  <button class="delete" aria-label="delete"></button>
-				</div>
-				<div class="message-body">
-				  ${dataItems.Message}
-				</div>
-			  </article>
-			`;
-					document.querySelector('.locationInfo').innerHTML = errorMessage;
-				} else {
-					showIcon('check');
-					const postoffice = dataItems.PostOffice[0];
-					const pos = Object.keys(postoffice).map(key => {
-						return postoffice[key] && (`
-				<div class="message-header mb-3">
-				  <p>${key}</p>
-				</div>
-				<div class="message-body">
-				  <strong>${postoffice[key]}</strong>
-				</div>
-			  `);
-					}).join('');
+}
+async function fetchData() {
+	try {
+		const response = await fetch(`https://api.postalpincode.in/pincode/${pincode.value}`);
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		return await response.json();
+	} catch (error) {
+		console.log('Error:', error.message);
+		// Handle the error appropriately
+	}
+}
+async function processData() {
+	try {
+		const data = await fetchData();
 
-					const successMessage = "<article class='message is-primary'>" + pos +
-						"</article>";
-					document.querySelector('.locationInfo').innerHTML = successMessage;
-				}
-			})
+		data.forEach(dataItems => {
+			if (dataItems.Status !== 'Success' || dataItems.Status === 404) {
+				showIcon('remove');
+				document.querySelector('.locationInfo').innerHTML = `
+          <article class="message is-danger">
+            <div class="message-header">
+              <p>Location</p>
+              <button class="delete" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+              ${dataItems.Message}
+            </div>
+          </article>
+        `;
+			} else {
+				showIcon('check');
+				const [post_office] = dataItems.PostOffice;
+				const pos = Object.keys(post_office).map(key => {
+					return post_office[key] && (`
+            <div class="message-header mb-3">
+              <p>${key}</p>
+            </div>
+            <div class="message-body">
+              <strong>${post_office[key]}</strong>
+            </div>
+          `);
+				}).join('');
+
+				document.querySelector('.locationInfo').innerHTML = "<article class='message is-primary'>" + pos +
+					"</article>";
+			}
 		});
+	} catch (error) {
+		console.log('Error:', error);
+		// Handle the error appropriately
+	}
 }
 
 const showIcon = (status) => {
@@ -61,7 +77,7 @@ const showIcon = (status) => {
 }
 
 const deleteText = (e) => {
-	if (e.target.className == 'delete') {
+	if (e.target.className === 'delete') {
 		document.querySelector('.locationInfo').remove();
 		document.querySelector('.pincode').value = '';
 		const checkIcon = document.querySelector(".icon-check");
